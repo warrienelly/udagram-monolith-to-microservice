fff1560ef390f52e287bc17f4d1fb80a37bd1e65
Warrie Warrie <warrie.warrieus@gmail.com>
2022-10-09 02:18:59 +0100

    New changes to frontend

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a84ee61d9b90047329e16103c687daa7-189242894.us-east-2.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
